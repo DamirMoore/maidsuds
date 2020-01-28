af317961708543ee2e6365eb41ed2f9638b98002
DamirMoore <damirmoore94@gmail.com>
2020-01-28 14:31:29 -0600

    made copyrite year dynamic /w auto-update. And made 'request quote' bar non-sticky for UI purposes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 function closeStickyEstimate() {
-    var sticky = document.querySelector('.free-estimate-sticky');
-    var closeButton = document.querySelector('.sticky-free-estimate-close');
+  var sticky = document.querySelector(".free-estimate-sticky");
+  var closeButton = document.querySelector(".sticky-free-estimate-close");
 
-    sticky.classList.remove('make-sticky');
-    closeButton.classList.add('hide-close-button');
-    console.log('sticky discovery closed');
+  sticky.classList.remove("make-sticky");
+  closeButton.classList.add("hide-close-button");
+  console.log("sticky discovery closed");
+}
+
+// changes the copyrite year automatically
+// takes html id of year <span> as argument
+function dynamicCopyrite(yearId) {
+  // dynamic copywrite year
+  var yearHtml = document.querySelector(`#${yearId}`);
+  var curYear = new Date().getFullYear();
+
+  // change year in <span> element
+  yearHtml.textContent = curYear;
+
+  // use fallback for textContent
+  if (yearHtml.innerHTML != curYear) {
+    yearHtml.innerText = curYear;
+  }
 }
